4d703f62bed86c45509a4d778c937f0998202cb4
Mickey_gl <hnytgl@163.com>
2026-06-07 16:04:04 +0800

    Add tests/WindowsSecurityChecker.Tests.ps1

---
+++ tests/WindowsSecurityChecker.Tests.ps1
@@ -0,0 +1,25 @@
+﻿$modulePath = Join-Path (Split-Path $PSScriptRoot -Parent) 'WindowsSecurityChecker.psm1'
+Import-Module $modulePath -Force
+
+Describe 'WindowsSecurityChecker module' {
+    It 'exports the main command' {
+        Get-Command Invoke-WindowsSecurityCheck -ErrorAction Stop | Should Not BeNullOrEmpty
+    }
+
+    It 'accepts the documented output formats' {
+        $command = Get-Command Invoke-WindowsSecurityCheck
+        $validateSet = $command.Parameters.Format.Attributes |
+            Where-Object { $_ -is [System.Management.Automation.ValidateSetAttribute] }
+        $formats = $validateSet.ValidValues
+        ($formats -contains 'Console') | Should Be $true
+        ($formats -contains 'Json') | Should Be $true
+        ($formats -contains 'Html') | Should Be $true
+        ($formats -contains 'All') | Should Be $true
+    }
+
+    It 'parses on Windows PowerShell' {
+        $errors = $null
+        [void][System.Management.Automation.Language.Parser]::ParseFile($modulePath, [ref]$null, [ref]$errors)
+        $errors.Count | Should Be 0
+    }
+}
